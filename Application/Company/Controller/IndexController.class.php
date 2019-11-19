d72931dd9ec04dd6be87c9d9cc93c81e370bede7
松哥 <228416481@qq.com>
2019-11-19 11:41:50 +0800

    1

--- Application/Company/Controller/IndexController.class.php
+++ Application/Company/Controller/IndexController.class.php
@@ -2,8 +2,21 @@
 namespace Company\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    //首页
     public function list(){
 
-        $this->display('index');
+        $this->display('index/index');
+    }
+    //发布
+    public function issue(){
+        $this->display('issue/issue');
+    }
+    //消息
+    public function msg(){
+        $this->display('msg/msg');
+    }
+    //我的
+    public function mine(){
+        $this->display('mine/mine');
     }
 }
