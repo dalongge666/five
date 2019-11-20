f70d4a27fda4ef023afcde039d334ce8c97c9b65
张顺 <l>
2019-11-20 10:51:19 +0800

    更新

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -9,4 +9,38 @@ class UserController extends Controller
     {
         $this -> display('user');
     }
+    public function userinfo(){
+
+        $this->display('user_info');
+    }
+    public function changePwd(){
+        if(IS_POST){
+            $member = D('Member');
+            $rule = array(
+                array('password','required','密码不能为空'),
+                array('newPwd','required','新密码不能为空'),
+                array('rePwd','required','确认密码不能为空'),
+                array('rePwd','password','两次密码不一致',0,'confirm'),
+            );
+            if($member->validate($rule)->create(I('post.'))){
+
+            }else{
+                $errors = $member->getError();
+                $this->assign('data',I('post'));
+                $this->assign('errors',$errors);
+                $this->display('user_info_mima');
+            }
+        }else{
+            $this->display('user_info_mima');
+        }
+
+
+    }
+
+    public function changeInfo(){
+
+        $this->display('user_info_person');
+    }
+
+
 }
