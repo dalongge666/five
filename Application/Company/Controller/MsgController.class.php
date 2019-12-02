bc9eef9a2b079e29b900e20e1e206741cf61712d
松哥 <228416481@qq.com>
2019-12-02 18:31:16 +0800

    1

--- Application/Company/Controller/MsgController.class.php
+++ Application/Company/Controller/MsgController.class.php
@@ -4,6 +4,11 @@ use Think\Controller;
 class MsgController extends Controller {
     //消息
     public function list(){
+        //查找求职
+
+
+//        $this -> assign('member',$member);
+//        $this -> assign('cr',$cr);
         $this->display('msg/msg');
     }
 }
